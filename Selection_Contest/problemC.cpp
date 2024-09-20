1e9b0bb161f3c7b04198befb55fbb618d576214e
TASIN <116006207+Muhtadina@users.noreply.github.com>
2024-09-21 00:42:04 +0600

    Final Commit

---
+++ Selection_Contest/problemC.cpp
@@ -0,0 +1,21 @@
+//C
+
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+int main()
+{
+    int A,B,C;
+    cin >> A>>B>>C;
+    if(C%2==0)
+    {
+        A = abs(A); B=abs(B);
+    }
+    if(A>B)
+        cout << ">" << endl;
+    else if(A<B)
+        cout << "<" << endl;
+    else
+        cout << "=" << endl;
+    return 0;
+}
