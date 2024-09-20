f0842c8df079205b92348ca46525f3bfbf274d10
TASIN <116006207+Muhtadina@users.noreply.github.com>
2024-09-21 00:52:43 +0600

    Final Commit

---
+++ Individual Contest Week-1/E.cpp
@@ -0,0 +1,14 @@
+//E
+
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+int main()
+{
+    int a,b; cin >> a >> b;
+    if(a<=b)
+        cout << a;
+    else
+        cout << a-1;
+    return 0;
+}
