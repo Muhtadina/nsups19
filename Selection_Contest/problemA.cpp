fff793ae90440484c7a59009768a9298b1e78fc3
TASIN <116006207+Muhtadina@users.noreply.github.com>
2024-09-21 00:39:09 +0600

    Final Commit

--- Selection_Contest/problemA.cpp
+++ Selection_Contest/problemA.cpp
@@ -1,1 +1,27 @@
+//A
 
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t; cin >> t;
+    while(t--)
+    {
+        int n, c=0; cin >> n;
+        string s;
+        cin >> s;
+        for(int i=0; i<n; i++)
+        {
+            if(s[i]=='m' && s[i+1]=='a' && s[i+2]=='p'){
+                c++;
+                i+=2;
+            }
+            else if(s[i]=='p' && s[i+1]=='i' && s[i+2]=='e'){
+                c++;
+                i+=2;
+            }
+        }
+        cout << c << endl;
+    }
+    return 0;
+}
